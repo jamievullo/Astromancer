7786b1b750320021dba1a0ffcd12e1d09ad28bef
jamievullo <jvullo@protonmail.com>
2019-12-30 20:59:38 -0500

    Added new pictures, event listener on pics, css styling

--- frontend/app.js
+++ frontend/app.js
@@ -1,2 +1,9 @@
-console.log('hello')
-
+// set variable to pic id for constellations
+const selectedConstellation = document.querySelectorAll('#pic')
+// iterate over pic collection and add event listener for which pic is being
+// selected(event.target). Can use constellation.target also since it gets iterated over.
+selectedConstellation.forEach(constellation => {
+    constellation.addEventListener('click', e => {
+        console.log(e.target)
+    })
+})
